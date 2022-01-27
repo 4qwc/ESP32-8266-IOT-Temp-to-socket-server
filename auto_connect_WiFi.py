7a24960e92097f083043c4b26cb2676416f8d13a
hs4qwc <hs4qwc@gmail.com>
2022-01-27 18:54:18 +0700

    Update auto_connect_WiFi.py

--- auto_connect_WiFi.py
+++ auto_connect_WiFi.py
@@ -1,10 +1,15 @@
+ssid = 'iPhone'
+password = 'aaa12345'
+
 def do_connect():
     import network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('กรุณาเชื่อมต่ออีกครั้ง...')
         sta_if.active(True)
-        sta_if.connect('<essid>', '<password>')
+        sta_if.connect(ssid, password)
     while not sta_if.isconnected():
         pass
     print('เครือข่าย IP:', sta_if.ifconfig())
+    
+do_connect()
